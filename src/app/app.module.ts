846c32658c6b63b718cc297cc8ad08c89fb2a999
Vinícius <viniolicar2004@gmail.com>
2022-03-28 14:44:17 -0300

    Component home e mais alguns ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeadComponent } from './components/head/head.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component'
+import { HomeComponent } from './views/home/home.component'
 
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,7 @@ import { MatListModule } from '@angular/material/list'
     MatIconModule,
     MatSidenavModule,
     MatListModule,
+    MatCardModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
